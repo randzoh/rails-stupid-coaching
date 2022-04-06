12c35efc8e3588763d399f3b574b9c7bc7024450
Randja Raharijaona <randzoh@outlook.com>
2022-04-05 21:29:29 -0400

    refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,6 @@
 class QuestionsController < ApplicationController
-
   def answer
     @questions = ['I am going to work', 'what time is it?']
-
     case params[:question]
     when 'hello' || 'hey'
       @answer = 'Hey!'
@@ -10,7 +8,7 @@ class QuestionsController < ApplicationController
       @answer = 'Great!'
     when @questions[1]
       @answer = Time.new.strftime('%I:%M %p')
-    when params[:question][-1] == '?'
+    when params[:question].last == '?'
       @answer = 'Silly question, get dressed and go to work!.'
     else
       @answer = "I don't care, get dressed and go to work!"
