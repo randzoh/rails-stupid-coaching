b165368872876d4a18c23b8ce3312291b91e4b4f
Randja Raharijaona <randzoh@outlook.com>
2022-04-05 20:42:39 -0400

    stupid coach app functional

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,21 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @questions = ['I am going to work', 'what time is it?']
+
+    case params[:question]
+    when 'hello' || 'hey'
+      @answer = 'Hey!'
+    when @questions[0]
+      @answer = 'Great!'
+    when @questions[1]
+      @answer = Time.new.strftime('%I:%M %p')
+    when params[:question][-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!.'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
